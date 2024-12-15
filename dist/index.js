7959f08e82618d4e343f594e010085f3270def8b
yanis900 <yanis991@hotmail.com>
2024-12-15 11:28:15 +0000

    main features

---
+++ dist/index.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+import OpenAI from "openai";
+import dotenv from "dotenv";
+import { readDirectories, writeFile } from "./fileUtils.js";
+dotenv.config();
+const openai = new OpenAI({
+    apiKey: process.env.OPEN_API_KEY,
+});
+const generateReadme = async () => {
+    const fileContent = await readDirectories();
+    if (!fileContent) {
+        console.log("Your src directory is empty!");
+    }
+    // console.log(fileContent);
+    try {
+      const completion = await openai.chat.completions.create({
+        model: "gpt-4o-mini",
+        messages: [
+          {
+            role: "user",
+            content: `Create a README file using markdown based on this src directory:\n${fileContent}`,
+          },
+        ],
+      });
+      const readmeContent = completion.choices[0].message.content;
+    //   console.log("Generated README:\n", readmeContent);
+      await writeFile(readmeContent);
+      console.log(`README as been created`)
+    } catch (err) {
+      console.error("Error generating README:", err);
+    }
+};
+generateReadme();
---
+++ fileUtils.ts
@@ -0,0 +1,32 @@
+import fs from "fs";
+import path from "path";
+
+const readDirectories = async () => {
+  try {
+    const files = await fs.promises.readdir("src");
+    let fileContent = "";
+    for (const file of files) {
+      const fullPath = path.join("src", file);
+      const data = await fs.promises.readFile(fullPath, "utf8");
+      fileContent += data + "\n";
+    }
+    return fileContent;
+  } catch (err) {
+    console.error("Error:", err);
+  }
+};
+
+const writeFile = async (content: string | null) => {
+  try {
+    if (typeof content === 'string') {
+      await fs.promises.writeFile('README.md', content);
+      console.log('File created successfully');
+    } else {
+      console.log('Content is not a string');
+    }
+  } catch (err) {
+    console.error('Error writing file:', err);
+  }
+};
+
+export { readDirectories, writeFile };
